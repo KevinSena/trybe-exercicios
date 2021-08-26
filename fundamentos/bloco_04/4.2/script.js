03b343f0146de2d09a27110412c1f77cbbeabd5d
Kevin Sena <senakevin50@gmail.com>
2021-08-26 19:24:07 -0300

    Task 4 completed

--- fundamentos/bloco_04/4.2/script.js
+++ fundamentos/bloco_04/4.2/script.js
@@ -31,3 +31,12 @@ for (let i = 0; i < numbers.length; i++) {
 }
 
 console.log(mediaAritmetica);
+
+// task 4
+
+console.log("\nCom o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: 'valor maior que 20'. Caso não seja, imprima a mensagem: 'valor menor ou igual a 20':")
+if (mediaAritmetica > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor que 20");
+}
