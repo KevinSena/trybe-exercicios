adf63a2beb08855c5929ceaa4478905f0d6a9814
Kevin Sena <senakevin50@gmail.com>
2021-08-27 15:51:28 -0300

    task 2 completed

--- fundamentos/bloco_04/4.3/script.js
+++ fundamentos/bloco_04/4.3/script.js
@@ -12,3 +12,14 @@ for (let i = 0; i < n; i += 1) {
 for (let i = 0; i < n; i += 1) {
   console.log(linha1)
 }
+
+// task 2
+
+console.log("\nImprime triângulo retângulo com 5 asteriscos de base");
+
+let linha2 = ''
+
+for (let i = 0; i < n; i += 1) {
+  linha2 += '*'
+  console.log(linha2)
+}
