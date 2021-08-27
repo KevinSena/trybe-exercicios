5789b7a2ca6106d3b37d9995c796161286b97515
Kevin Sena <senakevin50@gmail.com>
2021-08-27 15:45:51 -0300

    task 1 completed

---
+++ fundamentos/bloco_04/4.3/script.js
@@ -0,0 +1,14 @@
+// task 1
+
+console.log("Imprime quadrado");
+
+let n = 5
+let linha1 = ''
+
+for (let i = 0; i < n; i += 1) {
+  linha1 += '*'
+}
+
+for (let i = 0; i < n; i += 1) {
+  console.log(linha1)
+}
