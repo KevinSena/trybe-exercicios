7fd317796d159d962d8c668633ce80dc84bffc43
Kevin Sena <senakevin50@gmail.com>
2021-09-03 22:22:57 -0300

    Task 5 and 6 completed

--- fundamentos/bloco_05/5.1/script.js
+++ fundamentos/bloco_05/5.1/script.js
@@ -11,6 +11,16 @@ function changeBgColor(element, color) {
   element[0].style.backgroundColor = color;
 }
 
+function changeCase(element, i, style) {
+  element[i].style.textTransform = style
+}
+
+function allP(element) {
+  for (const iterator of element) {
+    console.log(iterator.innerText);
+  }
+}
+
 changeText(paragraph, 1, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum corrupti quo dicta cupiditate error fugiat deserunt, doloremque ipsam asperiores aspernatur nihil voluptates sapiente sunt quia libero repellendus excepturi mollitia!')
 
 changeBgColor(mainContent, 'rgb(76,164,109)')
@@ -18,3 +28,7 @@ changeBgColor(mainContent, 'rgb(76,164,109)')
 changeBgColor(centerContent, 'white')
 
 changeText(title, 0, 'Exercício 5.1 - JavaScript')
+
+changeCase(paragraph, 1, 'uppercase')
+
+allP(paragraph)
