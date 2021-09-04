4c89fda90c1195b5c025af8bb506450c95c8c36e
Kevin Sena <senakevin50@gmail.com>
2021-09-03 22:08:20 -0300

    Task 4 completed

--- fundamentos/bloco_05/5.1/script.js
+++ fundamentos/bloco_05/5.1/script.js
@@ -1,17 +1,20 @@
 let paragraph = document.getElementsByTagName('p')
 let mainContent = document.getElementsByClassName('main-content')
 let centerContent = document.getElementsByClassName('center-content')
+let title = document.getElementsByClassName('title')
 
-function changeText(element, text) {
-  element[1].innerText = text;
+function changeText(element, i ,text) {
+  element[i].innerText = text;
 }
 
 function changeBgColor(element, color) {
   element[0].style.backgroundColor = color;
 }
 
-changeText(paragraph, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum corrupti quo dicta cupiditate error fugiat deserunt, doloremque ipsam asperiores aspernatur nihil voluptates sapiente sunt quia libero repellendus excepturi mollitia!')
+changeText(paragraph, 1, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum corrupti quo dicta cupiditate error fugiat deserunt, doloremque ipsam asperiores aspernatur nihil voluptates sapiente sunt quia libero repellendus excepturi mollitia!')
 
 changeBgColor(mainContent, 'rgb(76,164,109)')
 
 changeBgColor(centerContent, 'white')
+
+changeText(title, 0, 'Exercício 5.1 - JavaScript')
