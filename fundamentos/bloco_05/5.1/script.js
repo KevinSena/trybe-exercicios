9298add95a953112b8e39b53977cc93164491f81
Kevin Sena <senakevin50@gmail.com>
2021-09-03 21:52:02 -0300

    Task 2 completed

--- fundamentos/bloco_05/5.1/script.js
+++ fundamentos/bloco_05/5.1/script.js
@@ -1,7 +1,14 @@
 let paragraph = document.getElementsByTagName('p')
+let mainContent = document.getElementsByClassName('main-content')
 
-function changeText(param, text) {
-  param[1].innerText = text;
+function changeText(element, text) {
+  element[1].innerText = text;
+}
+
+function changeBgColor(element, color) {
+  element[0].style.backgroundColor = color
 }
 
 changeText(paragraph, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum corrupti quo dicta cupiditate error fugiat deserunt, doloremque ipsam asperiores aspernatur nihil voluptates sapiente sunt quia libero repellendus excepturi mollitia!')
+
+changeBgColor(mainContent, 'rgb(76,164,109)')
