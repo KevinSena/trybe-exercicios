631443a7f11b3354ad36d86b528b4bffa54a570e
Kevin Sena <senakevin50@gmail.com>
2021-09-04 12:28:46 -0300

    Task 3 completed

--- fundamentos/bloco_05/5.2/script.js
+++ fundamentos/bloco_05/5.2/script.js
@@ -1,4 +1,6 @@
 let body = document.body
+let main = document.body.getElementsByTagName('main')
+let section = document.body.getElementsByTagName('section')
 
 function appendToChild(element, tag, text, className){
   let stuf = document.createElement(tag)
@@ -8,3 +10,5 @@ function appendToChild(element, tag, text, className){
 
 appendToChild(body, 'h1', 'Exercício 5.2 - JavaScript DOM')
 appendToChild(body, 'main', '', 'main-content')
+appendToChild(main[0], 'section', '', 'center-content')
+appendToChild(section[0], 'p', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum, adipisci harum eaque placeat quaerat ipsam omnis fugit praesentium debitis rerum at fuga error quas aliquam, a nesciunt corrupti unde?')
