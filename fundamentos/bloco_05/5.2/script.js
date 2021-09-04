cb6387ccc9eb74b21e3c827bfaa0d3ae5dfd6418
Kevin Sena <senakevin50@gmail.com>
2021-09-04 13:21:21 -0300

    Task 8 completed

--- fundamentos/bloco_05/5.2/script.js
+++ fundamentos/bloco_05/5.2/script.js
@@ -19,21 +19,22 @@ function tag(params) {
   return document.body.getElementsByTagName(params)
 }
 
-function loopElements(element, tag, array) {
+function loopElements(element, tag, array, className) {
   for (const iterator of array) {
-    appendToChild(element, tag, iterator)
+    appendToChild(element, tag, iterator, className)
   }
 }
 
-appendToChild(body, 'h1', 'Exercício 5.2 - JavaScript DOM')
+appendToChild(body, 'h1', 'Exercício 5.2 - JavaScript DOM', 'Titulo')
 appendToChild(body, 'main', '', 'main-content')
 appendToChild(main[0], 'section', '', 'center-content')
 appendToChild(main[0], 'section', '', 'left-content')
 appendToChild(main[0], 'section', '', 'right-content')
-appendToChild(section[0], 'p', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum, adipisci harum eaque placeat quaerat ipsam omnis fugit praesentium debitis rerum at fuga error quas aliquam, a nesciunt corrupti unde?')
+appendToChild(section[0], 'p', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum, adipisci harum eaque placeat quaerat ipsam omnis fugit praesentium debitis rerum at fuga error quas aliquam, a nesciunt corrupti unde?', 'paragraph')
 appendToChild(section[1], 'img', '', 'small-image')
-appendToChild(section[2], 'ul', '')
+appendToChild(section[2], 'ul', '', 'unorded_list')
 
 putSrc(img[0], "https://picsum.photos/200")
 
 loopElements(ul[0], 'li', listItems)
+loopElements(main[0], 'h3', Array(3).fill(''), 'Titulo_3')
