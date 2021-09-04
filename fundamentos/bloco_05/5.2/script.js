070ed62a40add23cec061744a1ce56c9281504ce
Kevin Sena <senakevin50@gmail.com>
2021-09-04 13:09:01 -0300

    Task 7 completed

--- fundamentos/bloco_05/5.2/script.js
+++ fundamentos/bloco_05/5.2/script.js
@@ -1,7 +1,9 @@
 let body = document.body
-let main = document.body.getElementsByTagName('main')
-let section = document.body.getElementsByTagName('section')
-let img = document.body.getElementsByTagName('img')
+let main = tag('main')
+let section = tag('section')
+let img = tag('img')
+let ul = tag('ul')
+let listItems = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez']
 
 function appendToChild(element, tag, text, className){
   let stuf = document.createElement(tag)
@@ -13,6 +15,16 @@ function putSrc(element, src) {
   element.src = src
 }
 
+function tag(params) {
+  return document.body.getElementsByTagName(params)
+}
+
+function loopElements(element, tag, array) {
+  for (const iterator of array) {
+    appendToChild(element, tag, iterator)
+  }
+}
+
 appendToChild(body, 'h1', 'Exercício 5.2 - JavaScript DOM')
 appendToChild(body, 'main', '', 'main-content')
 appendToChild(main[0], 'section', '', 'center-content')
@@ -20,4 +32,8 @@ appendToChild(main[0], 'section', '', 'left-content')
 appendToChild(main[0], 'section', '', 'right-content')
 appendToChild(section[0], 'p', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum, adipisci harum eaque placeat quaerat ipsam omnis fugit praesentium debitis rerum at fuga error quas aliquam, a nesciunt corrupti unde?')
 appendToChild(section[1], 'img', '', 'small-image')
+appendToChild(section[2], 'ul', '')
+
 putSrc(img[0], "https://picsum.photos/200")
+
+loopElements(ul[0], 'li', listItems)
