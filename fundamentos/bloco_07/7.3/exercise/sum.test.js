0f2bd137c26ff87170f0f560aa81d309763f548b
Kevin Sena <senakevin50@gmail.com>
2021-09-28 21:50:47 -0300

    adiciona tests, describe 1 e 2

--- fundamentos/bloco_07/7.3/exercise/sum.test.js
+++ fundamentos/bloco_07/7.3/exercise/sum.test.js
@@ -1,8 +1,21 @@
-const sum = require('./sum');
+const { sum, myRemove } = require('./sum');
+// const myRemove = require('./sum')
 
 describe('1 - A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
-  test('Teste se o retorno de sum(4, 5) é 9', () => expect(sum(1, 2)).toBe(3));
-  test('Teste se o retorno de sum(0, 0) é 0', () => expect(sum(0, 0)).toBe(0));
-  test('Teste se a função sum lança um erro quando os parâmetros são 4 e "5"', () => expect(() => {sum(4, '5')}).toThrow());
-  test('Teste se a mensagem de erro é "parameters must be numbers"', () => expect(() => {sum(4, '5')}).toThrowError(new Error('parameters must be numbers')));
+  test('Testa se o retorno de sum(4, 5) é 9', () => expect(sum(1, 2)).toBe(3));
+  test('Testa se o retorno de sum(0, 0) é 0', () => expect(sum(0, 0)).toBe(0));
+  test('Testa se a função sum lança um erro quando os parâmetros são 4 e "5"', () => expect(() => {sum(4, '5')}).toThrow());
+  test('Testa se a mensagem de erro é "parameters must be numbers"', () => expect(() => {sum(4, '5')}).toThrowError(new Error('parameters must be numbers')));
+});
+
+describe('2 - Recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array', () => {
+  test('Verifica se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  })
+  test('Verifica se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  })
+  test('Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  })
 });
