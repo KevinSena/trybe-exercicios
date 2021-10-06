9448455cdbc10ec552cf7167831e796c7d478cf0
Kevin Sena <senakevin50@gmail.com>
2021-10-06 20:00:21 -0300

    Adiciona função authorBornIn1947

---
+++ fundamentos/bloco_08/8.2/index.test.js
@@ -0,0 +1,6 @@
+const {authorBornIn1947} = require('./index');
+
+describe('Lista de Livros', () => {
+  test('Encontra o nome da primeira pessoa autora do livro nascida no ano de 1947.', () => expect(authorBornIn1947()).toEqual('Stephen King'))
+  test('Retorne o nome do livro de menor nome.', () => expect(smallerName()).toEqual('Duna'))
+});
