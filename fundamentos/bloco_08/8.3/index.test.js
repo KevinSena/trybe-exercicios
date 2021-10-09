2ab596af656dffe158886ef2fde42e9624341aab
Kevin Sena <senakevin50@gmail.com>
2021-10-09 18:56:26 -0300

    Realiza função formatedBookNames

---
+++ fundamentos/bloco_08/8.3/index.test.js
@@ -0,0 +1,15 @@
+const {formatedBookNames} = require('./index')
+
+describe('Usando map e filter', () => {
+  it('Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA', () => {
+    const expectedResult = [
+      'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+      'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+      'Fundação - Ficção Científica - Isaac Asimov',
+      'Duna - Ficção Científica - Frank Herbert',
+      'A Coisa - Terror - Stephen King',
+      'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+    ];
+    expect(formatedBookNames()).toEqual(expectedResult)
+  })
+})
