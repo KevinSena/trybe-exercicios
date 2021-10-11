9d1a5b43fa12dff8aab7ef5de10906290a5ef2c5
Kevin Sena <senakevin50@gmail.com>
2021-10-11 20:18:49 -0300

    finaliza rectangle.ls

--- fundamentos/bloco_08/8.5/rectangle.js
+++ fundamentos/bloco_08/8.5/rectangle.js
@@ -6,6 +6,5 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  rectangleArea();
-  console.log(rectangle[0] * rectangle[1]);
+  console.log(rectangleArea(...rectangle));
 });
