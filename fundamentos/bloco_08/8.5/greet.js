996a76021b764e23e42221d6729f3028725d126f
Kevin Sena <senakevin50@gmail.com>
2021-10-11 21:33:03 -0300

    finaliza greet.js

---
+++ fundamentos/bloco_08/8.5/greet.js
@@ -0,0 +1,5 @@
+const greet = (person, hi = 'Hi') => `${hi} ${person}`;
+
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
