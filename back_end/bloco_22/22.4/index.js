c673db68d285580a9dc7eacd55fe7216e8988e8d
Kevin Sena <senakevin50@gmail.com>
2022-03-16 21:23:01 -0300

    feat: put users requisition

--- back_end/bloco_22/22.4/index.js
+++ back_end/bloco_22/22.4/index.js
@@ -22,3 +22,8 @@ app.post('/greetings', (req, res) => {
   age > 17 ? res.status(200).json({ "message": `Hello, ${name}!` }) :
   res.status(401).json({ "message": "Unauthorized" });
 });
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(201).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` } )
+});
