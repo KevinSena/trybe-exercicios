fc32fd676c297589c9f7437d949bc0e04b5484a6
Kevin Sena <senakevin50@gmail.com>
2022-03-16 21:03:35 -0300

    feat: add greetings requisition

--- back_end/bloco_22/22.4/index.js
+++ back_end/bloco_22/22.4/index.js
@@ -4,15 +4,21 @@ const PORT = 3001;
 
 app.use(express.json())
 
+app.listen(PORT, () => {
+  console.log('Aplicação rodando na porta 3001\n',`http://localhost:${PORT}`);
+})
+
 app.get('/ping', (_req, res) => {
-  res.status(201).json({ message: 'pong' });
+  res.status(200).json({ message: 'pong' });
 });
 
 app.post('/hello', (req, res) => {
   const { name } = req.body;
-  res.status(201).json({ "message": `Hello, ${name}!` });
+  res.status(200).json({ "message": `Hello, ${name}!` });
 });
 
-app.listen(PORT, () => {
-  console.log('Aplicação rodando na porta 3001\n',`http://localhost:${PORT}`);
-})
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  age > 17 ? res.status(200).json({ "message": `Hello, ${name}!` }) :
+  res.status(401).json({ "message": "Unauthorized" });
+});
