31ba5e92042cc4ad93907cc6ef7f013e3e61ba25
Kevin Sena <senakevin50@gmail.com>
2022-03-17 16:41:13 -0300

    feat: make simpsons requisitions

--- back_end/bloco_22/22.4/index.js
+++ back_end/bloco_22/22.4/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const fs = require('fs/promises');
 const app = express();
 const PORT = 3001;
+const simpsons = require('./simpsons.json');
 
-app.use(express.json())
-
-app.listen(PORT, () => {
-  console.log('Aplicação rodando na porta 3001\n',`http://localhost:${PORT}`);
-})
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
@@ -26,4 +24,29 @@ app.post('/greetings', (req, res) => {
 app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
   res.status(201).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` } )
+});
+
+app.get('/simpsons', (_req, res) => {
+  res.status(200).send(simpsons);
+});
+
+app.get('/simpsons/:id', (req, res) => {
+  const { id } = req.params;
+  const charac = simpsons.find((e) => e.id == id);
+  charac ? res.status(200).send(charac.name) :
+    res.status(404).json({ message: 'simpson not found' });
+});
+
+app.post('/simpsons', ({ body }, res) => {
+  const { id, name } = body;
+  if(simpsons.find(e => e.id == id) || simpsons.find(e => e.name == name)){
+    return res.status(409).json({ message: 'id already exists' });
+  };
+  simpsons.push({id, name});
+  fs.writeFile('simpsons.json', JSON.stringify(simpsons), 'utf-8');
+  res.status(204).end();
+});
+
+app.listen(PORT, () => {
+  console.log('Aplicação rodando na porta 3001\n',`http://localhost:${PORT}`);
 });
