bc67dac671bceccfd833d4ffd8e77a54c4f5d88d
Kevin Sena <senakevin50@gmail.com>
2022-03-23 21:31:07 -0300

    feat: add error for getById

--- back_end/bloco_23/23.1/index.js
+++ back_end/bloco_23/23.1/index.js
@@ -20,6 +20,10 @@ app.get('/user', async (_req, res) => {
 app.get('/user/:id', async (req, res) => {
   const { id } = req.params;
   const user = await getById(id);
+  if (user.length === 0) return res.status(404).json({
+      "error": true,
+      "message": "Usuário não encontrado"
+  });
   return res.status(200).json(user);
 });
 
